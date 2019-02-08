211907282e2643faa9f44455f718bb64b5d151ca
mattie <mattie@Matties-MacBook-Pro.local>
2019-02-08 13:11:32 -0500

    project 1: improved code commenting

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -4,6 +4,8 @@ Game Shelving Simulator
 By Mattie KA
 
 Have you ever wondered what it's like to work in video game retail and to have to shelve dozens and dozens of near-obsolete games?? Wonder no longer!
+Drag and drop games from the "shelf" to the "grid" and alphabetize them.
+
 
 ******************/
 "use strict";
@@ -13,19 +15,24 @@ Have you ever wondered what it's like to work in video game retail and to have t
                                 VARIABLES
 ******************************************************************************/
 
+// Variables for counter and shelf
 let $counter;
 let $shelf;
-let waitTime = 1000;
+
+// Array for game titles
 let gameListArray = [];
+
+// SFX variables
 let stackSFX = new Audio ("assets/sounds/gameOnCounter.wav");
 let shelfSFX = new Audio ("assets/sounds/moveGame.wav");
 
+// Run setup when document is fully loaded
 $(document).ready(setup);
 
 /******************************************************************************
                                   SETUP
 ******************************************************************************/
-
+// setup function that allows dragging and dropping to, from, and between two jquery UI lists, loads game names, and opens up the instructions dialog box.
 function setup() {
   $counter = $('#counter');
   $shelf = $('#shelf');
@@ -33,14 +40,15 @@ function setup() {
   addToShelf();
   addToCounter();
 
+// loads list of games from text file into an array, then pulls 5 random titles from that list and loads them into the <p> tag of each list item of the five default games.
   $.get("/gamesList.txt", function(data){
     gameListArray = data.split("\n");
-    console.log(gameListArray);
     $("li p").each(function(){
       $(this).text(gameListArray[Math.floor(Math.random() * gameListArray.length-1)])
     })
 });
 
+// sends a dialog box containing instructions to the user. Closing the dialog box triggers two functions that will generate "customer" dialog boxes and new games to be added to the counter stack.
   $('#instructionsDialog').dialog({
     modal: true,
     dialogClass: "no-close",
@@ -58,6 +66,8 @@ function setup() {
                               ALLOW CONNECTION
 ******************************************************************************/
 
+
+// function that allows the user to drag and drop list items between two different lists. Learned this code with the jquery UI library documentation.
 function allowConnection() {
   $counter.sortable({
     connectWith: ".connectedSortable"
@@ -71,6 +81,7 @@ function allowConnection() {
                                 SHELF DROPPABLE
 ******************************************************************************/
 
+// function that makes the shelf(grid) droppable. When a list item is dropped there, its class becomes "gridForm". If it came from the counter list, the spine text is replaced with the cover text. A sound effect is also played when a game is dropped.
 function addToShelf() {
   $shelf.droppable({
     drop: function(event,ui) {
@@ -78,7 +89,6 @@ function addToShelf() {
       ui.draggable.find("p").removeClass("spineText").addClass("coverText");
       console.log(ui.draggable.attr("class"));
       shelfSFX.play();
-      //console.log(waitTime);
     }
   })
 }
@@ -86,6 +96,8 @@ function addToShelf() {
 /******************************************************************************
                               COUNTER DROPPABLE
 ******************************************************************************/
+
+// function that makes the counter list droppable. When a list item is dropped there, its class becomes "listForm". If it came from the shelf grid, the cover text is replaced with the spine text. A sound effect is also played when a game is dropped.
  function addToCounter() {
    $counter.droppable({
      drop: function(event,ui) {
@@ -97,18 +109,11 @@ function addToShelf() {
    })
  }
 
- /******************************************************************************
-                          RANDOM NUMBER FOR INTERVAL
- ******************************************************************************/
-//
-// function generateWaitTime() {
-//   waitTime = Math.floor(Math.random() * (5000-1000) + 1000);
-//   return waitTime;
-// }
-
 /******************************************************************************
                             NEW GAME LIST ITEM
 ******************************************************************************/
+
+// function that adds the next game to the counter. It grabs a title from the games list array, then creates a new list item with the list form and spine text classes. The title pulled from the array is displayed, and a sound effect plays. The setTimeout function calculates a random time to generate another game. See below for code source.
 
 function newGameDiv() {
   let $nextTitle = (gameListArray[Math.floor(Math.random() * gameListArray.length-1)]);
@@ -122,6 +127,8 @@ function newGameDiv() {
 /******************************************************************************
                             BRING UP NEW DIALOG BOX
 ******************************************************************************/
+
+// function that periodically spawns a new dialog box with the dialog text class. A message is appended to it. When the dialog box is closed, the text is removed so that the text doesn't get added over and over every time a new one is generated. Timeout function at the end also randomizes the timing of the creation of these dialog boxes.
 
 function generateDialog() {
   let $dialogText = $("#dialogText");
@@ -145,6 +152,9 @@ function generateDialog() {
                                   SOURCES
 ******************************************************************************/
 /*
+Set Timeout function for random time learned from here:
+https://stackoverflow.com/questions/34656758/javascript-setinterval-with-random-time
+
 Game Case Front: http://www.canbum.net/cdn/18/1991/949/xbox-game-cover-template_134936.jpg
 Game Case Spine: http://www.canbum.net/cdn/18/1991/949/xbox-360-game-cover-template_134914.png
 */
